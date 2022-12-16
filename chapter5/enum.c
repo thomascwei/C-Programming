9ab442b4aae0771497f8f6950fc161bc456a63a9
thomascwei <thomas.c.wei@gmail.com>
2022-12-16 09:20:25 +0800

    .

--- chapter5/enum.c
+++ chapter5/enum.c
@@ -3,20 +3,22 @@
 int main(int argc, char const *argv[])
 {
   int numberOfArgc;
-  enum gender{male, female};
-  enum gender myGender=male;
+  enum gender
+  {
+    male,
+    female
+  };
+  enum gender myGender = male;
   char myCharacter = 'K';
   printf("my gender=%d\n", myGender);
   printf("Gender=%c\n", myCharacter);
 
-  numberOfArgc=argc;
-  char *arg0=argv[0];
-  char *arg1=argv[1];
+  numberOfArgc = argc;
+  char *arg0 = argv[0];
+  char *arg1 = argv[1];
   printf("Number of arguments:%i\n", numberOfArgc);
   printf("Arguments1: %s\n", arg0);
   printf("first entry is %s\n", arg1);
-  
+
   return 0;
 }
-
-
